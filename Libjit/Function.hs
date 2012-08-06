3e86e63ad584e7037810d5dc6aacfe1258a75f19
Joel Svensson <svenssonjoel@yahoo.se>
2012-08-06 12:11:03 +0200

    experimenting with function building interface

--- Libjit/Function.hs
+++ Libjit/Function.hs
@@ -2,7 +2,11 @@
 
 {-# LANGUAGE GeneralizedNewtypeDeriving #-} 
 
-module Libjit.Function where
+module Libjit.Function ( ABI(..)   -- Raw
+                       , Function  -- Raw
+                       , buildFun, runFunBuilder
+                       , getParam
+                       , add, mul, ret ) where
 
 
 -- function building...
@@ -10,7 +14,8 @@ module Libjit.Function where
    - Function building utilities. 
 -} 
 
-import Libjit.Raw
+import qualified Libjit.Raw as Raw
+import Libjit.Raw (Function, ABI(..), Value, Context, Type)
 import Libjit.Jit
 
 import Control.Monad.State 
@@ -23,6 +28,30 @@ newtype FunBuilder a =
   FunBuilder (StateT FunStack Jit a)
    deriving (Monad, MonadIO, MonadState FunStack)
 
+top :: FunBuilder Function 
+top =
+  do
+    stack <- get
+    case stack of
+      [] -> error "Empty function stack"
+      (x:xs) -> return x
+
+push :: Function -> FunBuilder ()
+push f =
+  do
+    stack <- get
+    put (f:stack)
+
+pop :: FunBuilder Function
+pop =
+  do
+    stack <- get
+    case stack of
+      [] -> error "Empty function stack"
+      (x:xs) -> do
+        put xs
+        return x 
+
 liftJit :: Jit a -> FunBuilder a
 liftJit = FunBuilder . lift
 
@@ -31,18 +60,37 @@ getContext =
   do (Just ctx) <- liftJit get
      return ctx 
 
+runFunBuilder :: FunBuilder a -> Jit a
+runFunBuilder (FunBuilder fb) = evalStateT fb [] 
+
 buildFun :: ABI -> Type -> [Type] -> FunBuilder () -> FunBuilder Function
 buildFun abi out_t in_t body =
   do
     -- TODO: The last int is really an enum type, fix this ! 
-    sig <- liftIO$ createTypeSignature abi out_t in_t nargs 1
+    sig <- liftIO$ Raw.createTypeSignature abi out_t in_t nargs 1
 
     ctx <- getContext
-    fun <- liftIO$ createFunction ctx sig
+    fun <- liftIO$ Raw.createFunction ctx sig
+    push fun
     body 
-           
+    fun <- pop 
     return fun 
     where
       nargs = length in_t
  
-    
+-- Lift instructions into FunBuilder.
+
+lift1 :: (Function -> a -> IO b) -> a -> FunBuilder b
+lift1 f a = do {fun <- top; liftJit $ liftIO $ f fun a}
+
+lift2 :: (Function -> a -> b -> IO c) -> a -> b -> FunBuilder c
+lift2 f a b = do {fun <- top; liftJit $ liftIO $ f fun a b} 
+
+getParam :: Int -> FunBuilder Value
+getParam i = do { fun <- top; liftJit $ liftIO $ Raw.getParam fun i}
+
+ret = lift1 Raw.ret
+add = lift2 Raw.add
+mul = lift2 Raw.mul
+
+
--- examples/mul_add.hs
+++ examples/mul_add.hs
@@ -1,8 +1,10 @@
-{- Tests -}
+{- Tests
+   Joel Svensson 2012 
+-}
 
 -- Compile:  ghc --make test.hs ./Libjit/cbits/o/extra.o -ljit 
 import Libjit
-import Libjit.Raw
+import qualified Libjit.Raw as Raw
 
 
 import Foreign.Ptr
@@ -19,30 +21,31 @@ main =
     (Just ctx) <- get
     -- change "getIntType" to not being in IO monad.
     -- maybe also change the name. 
-    int_type <- liftIO$ getIntType 
-    liftIO$ startBuild(ctx)
-    sig <- liftIO$ createTypeSignature CDECL
-                               int_type
-                               [int_type,int_type,int_type]
-                               3 1
-    fun <- liftIO$ createFunction ctx sig
-    a <- liftIO$ getParam fun 0
-    b <- liftIO$ getParam fun 1
-    c <- liftIO$ getParam fun 2
-    tmp1 <- liftIO$ mul fun a b
-    tmp2 <- liftIO$ add fun tmp1 c
-    liftIO$ ret fun tmp2
-    liftIO$ compile fun 
-    liftIO$ endBuild ctx 
+    int_type <- liftIO$ Raw.getIntType 
+    liftIO$ Raw.startBuild(ctx)
+
+    fun <- runFunBuilder $ buildFun CDECL
+                                    int_type
+                                    [int_type, int_type,int_type] $
+      do
+        a <- getParam 0
+        b <- getParam 1
+        c <- getParam 2
+        tmp1 <- mul a b
+        tmp2 <- add tmp1 c
+        ret tmp2
+
+    liftIO$ Raw.compile fun 
+    liftIO$ Raw.endBuild ctx 
 
     -- Hackity
     liftIO$ withArray [3,5,2,0 :: Int] $ \ ptr ->
       do 
         let ptr' = castPtr ptr 
-        apply fun [ptr'
-                  ,(plusPtr ptr' 8) 
-                  ,(plusPtr ptr' 16)]
-                  (plusPtr  ptr' 24)
+        Raw.apply fun [ptr'
+                      ,(plusPtr ptr' 8) 
+                      ,(plusPtr ptr' 16)]
+                      (plusPtr  ptr' 24)
         val <- peek (castPtr (plusPtr ptr' 24))    
         putStrLn $ "result: " ++ show (val :: Int)
 
